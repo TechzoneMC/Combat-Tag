01f7cbe446ca8b915dfa8b0c852d7633aaf99de4
orkun1675 <orkun1675@hotmail.com>
2015-01-19 17:12:40 +0200

    Move to CompatibilityListener
    cancel damage if either players are not authenciated

--- src/main/java/techcable/minecraft/combattag/listeners/CompatibilityListener.java
+++ src/main/java/techcable/minecraft/combattag/listeners/CompatibilityListener.java
@@ -15,4 +15,14 @@ public class CompatibilityListener implements Listener {
 	public void onCombatTagByPlayer(CombatTagByPlayerEvent event) {
 		if (!event.getCTAttacker().isAuthenticated()) event.setCancelled(true);
 	}
+	@EventHandler(ignoreCancelled=true)
+     	public void onDamage(EntityDamageByEntityEvent event) {
+     		if ((event.getEntity() instanceof Player) && !event.getEntity().isAuthenticated()) {
+            		event.setCancelled(true);
+			return;
+        	}
+        	if ((Utils.getRootDamager(event.getDamager()) != null) && (Utils.getRootDamager(event.getDamager()) instanceof Player) && !Utils.getRootDamager(event.getDamager()).isAuthenticated()) {
+            		event.setCancelled(true);
+        	}
+     	}
 }
