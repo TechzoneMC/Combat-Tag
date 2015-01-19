d732ad31fdd5a474221973d38f5d1db44a4c777e
orkun1675 <orkun1675@hotmail.com>
2015-01-19 17:13:32 +0200

    Delete code that has been moved elsewhere

--- src/main/java/techcable/minecraft/combattag/listeners/PlayerListener.java
+++ src/main/java/techcable/minecraft/combattag/listeners/PlayerListener.java
@@ -54,15 +54,6 @@ public class PlayerListener implements Listener {
     }
     @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
     public void onDamage(EntityDamageByEntityEvent event) {
-        if ((event.getEntity() instanceof Player) && !event.getEntity().isAuthenticated()) {
-            event.setCancelled(true);
-            return;
-        }
-        if ((Utils.getRootDamager(event.getDamager()) != null) && (Utils.getRootDamager(event.getDamager()) instanceof Player) && !Utils.getRootDamager(event.getDamager()).isAuthenticated()) {
-            event.setCancelled(true);
-            return;
-        }
-        
     	if (!(event.getEntity() instanceof Player)) return; //Not a player
     	CombatTagPlayer defender = CombatTagPlayer.getPlayer(((Player)event.getEntity()).getUniqueId());
     	LivingEntity attacker = Utils.getRootDamager(event.getDamager());
